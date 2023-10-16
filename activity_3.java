88b5be601175dc785fcc9c1c82da3d47346249a0
RMIT-s3661446 <s3661446@student.rmit.edu.au>
2023-10-16 22:34:07 +1100

    Activity 3 code correct

--- activity_3.java
+++ activity_3.java
@@ -9,8 +9,12 @@ import java.util.List;
 public class Department {
     
     private String name;
+
+
+    //Problem: Naming conventions make it hard to identify elements. For example, instead naming the arraylist of IDs "ID1", it's better to have it named IDArray or similar to better indicate its purpose
+    //A lot of the functions use "this.". This step invokes the constructor, which can make functions work incorrectly
     
-    private ArrayList<String> ID1 = new ArrayList<String>();
+    private ArrayList<String> IDArray = new ArrayList<String>();
     
     private ArrayList<String> temp1 = new ArrayList<String>();
 
@@ -20,6 +24,7 @@ public class Department {
      * ORIGINAL:
      * private ArrayList<Integer> employee = new ArrayList<Integer>();
      */
+
 
     
     private ArrayList<Integer> employee = new ArrayList<Integer>();
@@ -39,32 +44,55 @@ public class Department {
     }
 
     
-    public boolean checkEmployee(int eID) {
+    public boolean checkEmployee(int eID) { //refactored
 
-	    int p=0;
-	    for (int i = 0; i < this.employee.size(); i++){
-        check=this.employee.get(i);
-            if (check==eID) {
-                p=1;
+        //ORIGINAL: int p=0;
+        //The function used int as a variable that could have been either 0 or 1. Boolean is much more fitting for it
+        //As a result, new boolean variable was created in its stead which can subsequently be returned
+        //The boolean is set to false by default
+
+	    
+        boolean employeeFound = false;
+	    for (int i = 0; i < employee.size(); i++){
+
+            //ORIGINAL: check=this.employee.get(i);
+            //It is not necessary to cache the current element as it's only used for comparison
+
+
+            if (employee.get(i) == eID) {
+                //ORIGINAL: p=1;  As mentioned earlier, "p" variable is now redundant, and instead, if the If statement is true, it will set the boolean to true
+                employeeFound = true;
             }
         }
 
-        if (p==1){
+
+        //Since a new boolean was added, there was and int p removed, there is no point at all in making the return depend on the if statement
+        //Instead it just checks if the earlier declared boolean is true to print
+
+    
+
+        if (employeeFound){
             System.out.println("Employee exists");
-            return true;
+            //return true
         }
         else{
             System.out.println("Employee doesn't exists");
-            return false;
+            //return false
 	    }
+
+
+        //The function returns the earlier boolean variable.
+        return employeeFound;
     }
 
     
 
     public void assignNew(string temp2, int ID) {
-        this.temp1.add(temp2);
-	  this.ID1.add(ID);
+        temp1.add(temp2);
+	    IDArray.add(ID);
     }
+
+
 
 
 }
